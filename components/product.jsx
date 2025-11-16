be6833a3afc3ec94f837b2205f7641e49c4f1e23
Raghab Pandit <bahgar55@gmail.com>
2025-11-16 17:19:27 +0545

    Adjust product component layout and styles

--- components/product.jsx
+++ components/product.jsx
@@ -6,8 +6,8 @@ import { FaArrowUpRightFromSquare } from 'react-icons/fa6'
 const Product = ({id, image, name, price}) => {
   return (
     <>
-        <Link href={`/products/${id}`} className='group h-60 sm:h-80 w-40 lg:w-60 shadow-[0_4px_30px_rgba(0,0,0,0.1)]'>
-            <div className="h-[75%]  relative overflow-hidden border border-transparent rounded-t-[20px] bg-white/80 group-hover:bg-white transition-all duration-300 flex items-end justify-center">
+        <Link href={`/products/${id}`} className='group h-68 sm:h-80 w-40 lg:w-60 shadow-[0_4px_30px_rgba(0,0,0,0.1)]'>
+            <div className="h-[70%]  relative overflow-hidden border border-transparent rounded-t-[20px] bg-white/80 group-hover:bg-white transition-all duration-300 flex items-end justify-center">
             <div className="absolute flex items-center justify-end w-full invisible group-hover:visible bottom-0">
                 <button className='bg-transparent border-1 border-transparent px-4 py-1 rounded-full text-[#5a24c9]/70 transition-colors duration-300 cursor-pointer'>
                     <FaArrowUpRightFromSquare />
@@ -21,7 +21,7 @@ const Product = ({id, image, name, price}) => {
                     height={90}
                 /> : <div className="h-full w-full flex items-center justify-center text-gray-400">No Image Available</div>}
             </div>
-            <div className="h-[25%] bg-[#1E2330] border border-transparent flex flex-col items-center rounded-b-[20px]">
+            <div className="h-[30%] bg-[#1E2330] border border-transparent flex flex-col items-center rounded-b-[20px]">
                 <div className="mt-2 flex flex-col items-center justify-between h-full px-2">
                     <h1 className='text-white font-semibold text-md sm:mt-2'>{name ? `${name.split(' ').filter((title, id)=> id < 4).join(' ')}` + `${name.split(' ').length > 4 ? ".." : ""}` : "Product Name"}</h1>
                 </div>            
